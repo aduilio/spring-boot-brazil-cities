ec18e9884e59700fe466d8450ee2370b381610b2
Aduilio Silva <developer.aduilio@gmail.com>
2021-05-11 11:34:54 -0300

    Start project

---
+++ src/main/java/com/aduilio/brazilcities/BrazilCitiesApplication.java
@@ -0,0 +1,13 @@
+package com.aduilio.brazilcities;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BrazilCitiesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BrazilCitiesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/aduilio/brazilcities/BrazilCitiesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aduilio.brazilcities;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BrazilCitiesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
